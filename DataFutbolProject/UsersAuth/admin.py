5ea43506dfb1d8953bdcded4a86b91f0691f9efd
Krawchik.Ivan <kwrawchiki@jefatura.gob.ar>
2023-05-16 19:15:17 -0300

    Correccion models

--- DataFutbolProject/UsersAuth/admin.py
+++ DataFutbolProject/UsersAuth/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Jugadores,Tecnicos,Equipos
 
 # Register your models here.
+class JugadoresAdmin(admin.ModelAdmin):
+    list_display=('equipo_id','nombre','posicion')
+
+class TecnicosAdmin(admin.ModelAdmin):
+    list_display=('equipo_id','nombre')
+
+class EquiposAdmin(admin.ModelAdmin):
+    list_display=('equipo_id','nombre')
+
+admin.site.register(Jugadores, JugadoresAdmin)
+admin.site.register(Tecnicos, TecnicosAdmin)
+admin.site.register(Equipos, EquiposAdmin)
